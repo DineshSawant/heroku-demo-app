16d23954ec413345d73ac57a1e1ba8e30ce39242
Dinesh Sawant <disawant@in.ibm.com>
2018-09-11 17:45:57 +0530

    Add basic route animations

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+
+import{ routeFadeStateTrigger, routeSlideStateTrigger } from '../../shared/route-animations';
 
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.css']
+  styleUrls: ['./aboutus.component.css'],
+  animations: [
+    routeFadeStateTrigger,
+    routeSlideStateTrigger
+  ]
 })
 export class AboutusComponent implements OnInit {
+  // @HostBinding('@routeFadeState') routeAnimation = true;
+  @HostBinding('@routeSlideState') routeAnimation = true;
 
   constructor() { }
 
